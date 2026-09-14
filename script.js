59311ec5e45c5295907dc0dccb5add3433a2fc1c
divafarah <divafrh04@gmail.com>
2026-09-14 19:12:04 +0700

    Update website profil

---
+++ script.js
@@ -0,0 +1,45 @@
+let halamanSekarang = 0;
+
+const halaman = document.querySelectorAll(".halaman");
+
+
+function tampilkanHalaman(index) {
+
+    halaman.forEach(function(item) {
+
+        item.classList.remove("aktif");
+
+    });
+
+    halaman[index].classList.add("aktif");
+
+    halamanSekarang = index;
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+}
+
+
+function nextHalaman() {
+
+    if (halamanSekarang < halaman.length - 1) {
+
+        tampilkanHalaman(halamanSekarang + 1);
+
+    }
+
+}
+
+
+function halamanSebelumnya() {
+
+    if (halamanSekarang > 0) {
+
+        tampilkanHalaman(halamanSekarang - 1);
+
+    }
+
+}
